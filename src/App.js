4fca99b47fa35b0a8d60357684c52b08a3e983bd
m1k0t0f667 <h.briffaux@gmail.com>
2023-10-18 11:03:53 +0200

    Tranquille

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl italic">Hello world!</h1>
+      <h1 className="text-3xl ">Hello world!</h1>
     </div>
   );
 }
