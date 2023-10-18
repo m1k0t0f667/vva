dcd11fba07b36dd99605512f47682c4feb12974a
Flavien-Walk <114407085+Flavien-Walk@users.noreply.github.com>
2023-10-18 14:37:43 +0200

    api relié avec prédiction vainqueur

--- api/main.py
+++ api/main.py
@@ -1,84 +1,76 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import csv
 from collections import Counter
 from starlette.responses import RedirectResponse
 
-
-
 app = FastAPI()
 
+# Middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-matches=[]
-next_matches=[]
+# Data Loading
+def load_data(file_name: str) -> list:
+    data = []
+    with open(file_name, 'r') as file:
+        csvreader = csv.DictReader(file)
+        for row in csvreader:
+            data.append(row)
+    return data
 
-with open("./oddRugby.csv", 'r') as file:
-    csvreader = csv.DictReader(file)
-    for row in csvreader: # each row is a list
-        matches.append(row)
-with open("./transformed_rugby.csv", 'r') as file:
-    csvreader = csv.DictReader(file)
-    for row in csvreader: # each row is a list
-        matches.append(row)
+matches = load_data("./oddRugby.csv") + load_data("./transformed_rugby.csv")
+next_matches = load_data("./transformed_new_rugby.csv")
 
-with open("./transformed_new_rugby.csv", 'r') as file:
-    csvreader = csv.DictReader(file)
-    for row in csvreader: # each row is a list
-        next_matches.append(row)
-
-
-def most_frequent(List):
-    occurence_count = Counter(List)
+# Utility Functions
+def most_frequent(data_list: list) -> str:
+    occurence_count = Counter(data_list)
     return occurence_count.most_common(1)[0][0]
 
+def get_winner_by_odd(match: dict) -> str:
+    if not match["odd_team1"]:
+        return None
 
+    if match["odd_team1"] > match["oddDraw"] and match["odd_team2"] > match["oddDraw"]:
+        return "Draw"
+    return match["team1"] if match["odd_team1"] <= match["odd_team2"] else match["team2"]
 
-def get_winner_by_odd(match):
-    if match["odd_team1"]!=None:
-        if match["odd_team1"]>match["oddDraw"] and match["odd_team2"]>match["oddDraw"]:
-            return "Draw"
-        elif match["odd_team1"]>match["odd_team2"]:
-            return match["team2"]
-        else:
-            return match["team1"]
+def get_winner_by_points(match: dict) -> str:
+    if not match["score1"]:
+        return None
 
-def get_winner_by_points(match):
-    if match["score1"]!=None:
-        if match["score1"]==match["score2"]:
-            return "Draw"
-        elif match["score1"]>match["score2"]:
-            return match["team1"]
-        else:
-            return match["team2"]
-        
-def get_winner_old(o_match):
-    r=[]
-    for omatch in matches:
-        if o_match["team1"] in omatch.values() and o_match["team2"] in omatch.values():
-            r.append(get_winner_by_points(omatch))
-    if len(r)>=10:
-        return most_frequent(r),most_frequent(r[0:10])
-    else :
-        return most_frequent(r),(r[0:-1])
+    if match["score1"] == match["score2"]:
+        return "Draw"
+    return match["team1"] if match["score1"] > match["score2"] else match["team2"]
 
+def get_winner_old(current_match: dict) -> tuple:
+    relevant_matches = [match for match in matches if current_match["team1"] in match.values() and current_match["team2"] in match.values()]
+    winners = [get_winner_by_points(match) for match in relevant_matches]
+    most_common = most_frequent(winners)
+    most_common_10 = most_frequent(winners[:10]) if len(winners) >= 10 else most_common
+
+    return most_common, most_common_10
+
+# API Endpoints
 @app.get("/{pays}")
-def get_allMatches(pays:str=None):
-    result=[]
-    n_result=[]
-    if pays :   
-        for i in matches:
-           if pays in i.values():
-               result.append(i)
-        for i in next_matches:
-           if pays in i.values():
-                i["winner_by_odd"]=get_winner_by_odd(i)
-                g,f=get_winner_old(i)
-                i["winner_by_all_matches"]=g
-                if len(result)>=10:
-                    i["winner_by_10_l_matches"]=f
-                else:
-                    i["winner_by_10_l_matches"]="Idem"
-                n_result.append(i)
-    return {"result10":result[0:10],"next_matches":n_result}
+def get_all_matches(pays: str) -> dict:
+    relevant_matches = [match for match in matches if pays in match.values()]
+    upcoming_matches = []
+
+    for match in next_matches:
+        if pays in match.values():
+            match["winner_by_odd"] = get_winner_by_odd(match)
+            overall, recent = get_winner_old(match)
+            match["winner_by_all_matches"] = overall
+            match["winner_by_10_l_matches"] = recent if len(relevant_matches) >= 10 else "Idem"
+            upcoming_matches.append(match)
+
+    return {"result10": relevant_matches[:10], "next_matches": upcoming_matches}
 
 # @app.get("/")
 # def get_prediction(country_1: str=None, country_2: str=None):
--- application/src/Pages/Page.js
+++ application/src/Pages/Page.js
@@ -1,35 +1,48 @@
 import React, { useState, useRef, useEffect } from 'react';
-import logo from '../logo.svg'; 
-import team1Image from '../Assets/france.png'; 
-import team2Image from '../Assets/portugal.png'; 
+import logo from '../logo.svg';
+import team1Image from '../Assets/france.png';
+import team2Image from '../Assets/portugal.png';
+import team3Image from '../Assets/england.png';
 
 function Page() {
     const [currentImage, setCurrentImage] = useState(logo);
     const [showDropdown, setShowDropdown] = useState(false);
     const logoRef = useRef(null);
     const dropdownRef = useRef(null);
+    const [prediction, setPrediction] = useState(null);
 
     const teams = [
-        { name: 'Équipe 1', image: team1Image },
-        { name: 'Équipe 2', image: team2Image },
-        // La tu ajoute t'es équipes 
+        { name: 'Équipe 1', image: team1Image, countryCode: 'france' },
+        { name: 'Équipe 2', image: team2Image, countryCode: 'portugal' },
+        { name: 'Équipe 3', image: team3Image, countryCode: 'England' },
+
+        // Ajoutez d'autres équipes et leurs codes de pays ici
     ];
 
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (showDropdown &&
-                logoRef.current && !logoRef.current.contains(event.target) &&
-                dropdownRef.current && !dropdownRef.current.contains(event.target)
-            ) {
-                setShowDropdown(false);
-            }
-        };
+    const handleImageChange = (teamImage, countryCode) => {
+        setCurrentImage(teamImage);
+        setShowDropdown(false); // Ferme le menu déroulant après avoir sélectionné une équipe
+        // Vous pourriez potentiellement utiliser countryCode pour la requête vers l'API
+    };
 
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [showDropdown]);
+    const handleStart = () => {
+        const selectedTeam = teams.find(team => team.image === currentImage);
+        if (selectedTeam) {
+            fetch(`http://127.0.0.1:8000/${selectedTeam.countryCode}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    const predictedWinner = data.next_matches[0].winner_by_all_matches;
+                    setPrediction(predictedWinner);
+                })
+                .catch(error => console.error('Erreur lors de la récupération des données:', error));
+        }
+    };
+    
 
     return (
         <div className="flex justify-center items-center h-screen bg-gray-200">
@@ -42,7 +55,6 @@ function Page() {
                         className="w-64 h-64 rounded-full cursor-pointer mx-auto"
                         onClick={() => setShowDropdown(!showDropdown)}
                     />
-
                     {showDropdown && (
                         <div ref={dropdownRef} className="absolute mt-5 w-64 rounded-md shadow-lg bg-white">
                             <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
@@ -52,12 +64,12 @@ function Page() {
                                         href="#" 
                                         className="flex items-center block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" 
                                         role="menuitem"
-                                        onClick={() => {
-                                            setCurrentImage(team.image);
-                                            setShowDropdown(false); // Ferme le menu déroulant
+                                        onClick={(e) => {
+                                            e.preventDefault(); // Pour éviter le comportement par défaut du lien
+                                            handleImageChange(team.image, team.countryCode);
                                         }}
                                     >
-                                        <img src={team.image} alt={team.name} className="w-10 h-10 rounded-full mr-3"/>
+                                        <img src={team.image} alt={team.name} className="w-10 h-10 rounded-full mr-3" />
                                         {team.name}
                                     </a>
                                 ))}
@@ -65,10 +77,10 @@ function Page() {
                         </div>
                     )}
                 </div>
-
-                <button className="mt-8 bg-blue-500 hover:bg-blue-600 text-white py-2 px-8 rounded-full mx-auto block">
+                <button className="mt-8 bg-blue-500 hover:bg-blue-600 text-white py-2 px-8 rounded-full mx-auto block" onClick={handleStart}>
                     Start
                 </button>
+                {prediction && <div className="mt-4 text-lg">{prediction}</div>}
             </div>
         </div>
     );
