b608c47ecb6e7439d685b2952a0e90aae66fff27
Flavien-Walk <114407085+Flavien-Walk@users.noreply.github.com>
2023-10-18 15:43:39 +0200

    page.js à jour avec menu déroulant + all teams

--- application/src/Pages/Page.js
+++ application/src/Pages/Page.js
@@ -3,26 +3,81 @@ import logo from '../logo.svg';
 import team1Image from '../Assets/france.png';
 import team2Image from '../Assets/portugal.png';
 import team3Image from '../Assets/england.png';
+import team4Image from '../Assets/Argentine.png';
+import team5Image from '../Assets/afrique du sud.png';
+import team6Image from '../Assets/Australie.png';
+import team7Image from '../Assets/Chilli.png';
+import team8Image from '../Assets/Ecosse.jpg';
+import team9Image from '../Assets/Fidji.jpg';
+import team10Image from '../Assets/Géorgie.png';
+import team11Image from '../Assets/Irlande.png';
+import team12Image from '../Assets/italie.png';
+import team13Image from '../Assets/Japon.png';
+import team14Image from '../Assets/Namibie.png';
+import team15Image from '../Assets/Nouvelle-Zélande.png';
+import team16Image from '../Assets/Pays de Galles.png';
+import team17Image from '../Assets/Roumanie.png';
+import team18Image from '../Assets/Samoa.png';
+import team19Image from '../Assets/Tonga.jpg';
+import team20Image from '../Assets/Uruguay.png';
+
+
+
+
+
+
+
 
 function Page() {
     const [currentImage, setCurrentImage] = useState(logo);
     const [showDropdown, setShowDropdown] = useState(false);
-    const logoRef = useRef(null);
-    const dropdownRef = useRef(null);
     const [prediction, setPrediction] = useState(null);
+    const dropdownRef = useRef(null);
+    const logoRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target) && !logoRef.current.contains(event.target)) {
+                setShowDropdown(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     const teams = [
-        { name: 'Équipe 1', image: team1Image, countryCode: 'france' },
-        { name: 'Équipe 2', image: team2Image, countryCode: 'portugal' },
-        { name: 'Équipe 3', image: team3Image, countryCode: 'England' },
+        { name: 'France', image: team1Image, countryCode: 'France' },
+        { name: 'Portugal', image: team2Image, countryCode: 'Portugal' },
+        { name: 'Angleterre', image: team3Image, countryCode: 'England' },
+        { name: 'Argentine', image: team4Image, countryCode: 'Argentina' },
+        { name: 'Afrique du Sud', image: team5Image, countryCode: 'South Africa' },
+        { name: 'Australie', image: team6Image, countryCode: 'Australia' },
+        { name: 'Chilli', image: team7Image, countryCode: 'Chile' },
+        { name: 'Ecosse', image: team8Image, countryCode: 'Scotland' },
+        { name: 'Fidji', image: team9Image, countryCode: 'Fiji' },
+        { name: 'Géorgie', image: team10Image, countryCode: 'Georgia' },
+        { name: 'Irlande', image: team11Image, countryCode: 'Ireland' },
+        { name: 'Italie', image: team12Image, countryCode: 'Italia' },
+        { name: 'Japon', image: team13Image, countryCode: 'Japan' },
+        { name: 'Namibie', image: team14Image, countryCode: 'Namibia' },
+        { name: 'Nouvelle-Zélande', image: team15Image, countryCode: 'New Zealand' },
+        { name: 'Pays de Galles', image: team16Image, countryCode: 'Wales' },
+        { name: 'Roumanie', image: team17Image, countryCode: 'Romania' },
+        { name: 'Samoa', image: team18Image, countryCode: 'Samoa' },
+        { name: 'Tonga', image: team19Image, countryCode: 'Tonga' },
+        { name: 'Uruguay', image: team20Image, countryCode: 'Uruguay' },
 
-        // Ajoutez d'autres équipes et leurs codes de pays ici
+        
+        // ... autres équipes
     ];
 
     const handleImageChange = (teamImage, countryCode) => {
         setCurrentImage(teamImage);
-        setShowDropdown(false); // Ferme le menu déroulant après avoir sélectionné une équipe
-        // Vous pourriez potentiellement utiliser countryCode pour la requête vers l'API
+        setShowDropdown(false);
+        setPrediction(null);
     };
 
     const handleStart = () => {
@@ -42,7 +97,6 @@ function Page() {
                 .catch(error => console.error('Erreur lors de la récupération des données:', error));
         }
     };
-    
 
     return (
         <div className="flex justify-center items-center h-screen bg-gray-200">
@@ -56,7 +110,7 @@ function Page() {
                         onClick={() => setShowDropdown(!showDropdown)}
                     />
                     {showDropdown && (
-                        <div ref={dropdownRef} className="absolute mt-5 w-64 rounded-md shadow-lg bg-white">
+                        <div ref={dropdownRef} className="absolute mt-5 w-64 rounded-md shadow-lg bg-white overflow-y-auto" style={{maxHeight: '300px'}}>
                             <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                                 {teams.map(team => (
                                     <a 
@@ -65,7 +119,7 @@ function Page() {
                                         className="flex items-center block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" 
                                         role="menuitem"
                                         onClick={(e) => {
-                                            e.preventDefault(); // Pour éviter le comportement par défaut du lien
+                                            e.preventDefault();
                                             handleImageChange(team.image, team.countryCode);
                                         }}
                                     >
