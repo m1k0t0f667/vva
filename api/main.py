0088a6bda666637d79e556f19d38efbf43c18419
m1k0t0f667 <h.briffaux@gmail.com>
2023-10-18 16:06:51 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -3,12 +3,16 @@ import csv
 from collections import Counter
 from starlette.responses import RedirectResponse
 import json
+<<<<<<< Updated upstream
 from fastapi.middleware.cors import CORSMiddleware
+=======
+>>>>>>> Stashed changes
 
 app = FastAPI()
 
 origins = ["http://localhost:3000"]
 
+<<<<<<< Updated upstream
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -20,6 +24,11 @@ app.add_middleware(
 matches = []
 next_matches = []
 
+=======
+matches = []
+next_matches = []
+
+>>>>>>> Stashed changes
 with open("./oddRugby.csv", "r") as file:
     csvreader = csv.DictReader(file)
     for row in csvreader:  # each row is a list
@@ -73,6 +82,18 @@ def get_winner_old(o_match):
     else:
         return most_frequent(r), (r[0:-1])
 
+<<<<<<< Updated upstream
+=======
+
+def moyenne(l):
+    r = 0
+    r1 = 0
+    for i in l:
+        r += int(i[0])
+        r1 += int(i[1])
+    return round(r / len(l)), round(r1 / len(l))
+
+>>>>>>> Stashed changes
 
 @app.get("/{pays}")
 def get_allMatches(pays: str = None):
@@ -92,11 +113,16 @@ def get_allMatches(pays: str = None):
                 else:
                     i["winner_by_10_l_matches"] = "Idem"
                 n_result.append(i)
+<<<<<<< Updated upstream
     return json.dumps({"result10": result[0:10], "next_matches": n_result})
+=======
+    return {"result10": result[0:10], "next_matches": n_result}
+>>>>>>> Stashed changes
 
 
 @app.get("/")
 def get_prediction(country_1: str = None, country_2: str = None):
+<<<<<<< Updated upstream
     result = {}
     if country_1 and country_2:
         for i in matches:
@@ -105,6 +131,25 @@ def get_prediction(country_1: str = None, country_2: str = None):
                     result["winner_by_matches_all"],
                     result["winner_by_matches_last"],
                 ) = get_winner_by_odd(i)
+=======
+    prediction_points = []
+    r = []
+    if country_1 and country_2:
+        for i in matches:
+            if country_1 in i.values() and country_2 in i.values():
+                r.append(i)
+                if country_1 == i["team1"]:
+                    prediction_points.append([i["score1"], i["score2"]])
+                else:
+                    prediction_points.append([i["score2"], i["score1"]])
+        h = get_winner_old({"team1": country_1, "team2": country_2})
+        return {
+            "result_by_points": moyenne(prediction_points),
+            "result_by_victory10": h[1],
+            "result_by_victory": h[0],
+            "10_last_matches": r[0:10],
+        }
+>>>>>>> Stashed changes
     elif country_1:
         redirect_url = f"/{country_1}"
         return RedirectResponse(redirect_url)
@@ -112,6 +157,7 @@ def get_prediction(country_1: str = None, country_2: str = None):
         redirect_url = f"/{country_2}"
         return RedirectResponse(redirect_url)
     else:
+<<<<<<< Updated upstream
         return "Please select a country or two country : http://0.0.0.0:80/?country_1=&country_2="
 
 
@@ -119,3 +165,6 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app, host="localhost", port=8080)
+=======
+        return "Please select a country or two country : http://127.0.0.1:8080/?country_1=&country_2="
+>>>>>>> Stashed changes
