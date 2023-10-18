20c3be6b17a048def41cb09a3d0dd4dddf5ee048
m1k0t0f667 <h.briffaux@gmail.com>
2023-10-18 14:45:10 +0200

    Slaut

--- application/src/App.js
+++ application/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [match, setMatch] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8080/England");
+        if (response.ok) {
+          const data = await response.json();
+          const data2 = JSON.parse(data);
+          setMatch(data2["result10"]); // Update the state with the fetched data
+        } else {
+          console.error("Failed to fetch data");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {Object.keys(match).map((key, i) => (
+          <p>{key}:</p>
+        ))}
+      </div>
     </div>
   );
 }
